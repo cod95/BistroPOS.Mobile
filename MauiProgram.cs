c7efb4e31ec05b55c387997f2abf728f2da83f09
cod95 <148455928+cod95@users.noreply.github.com>
2026-08-23 09:57:30 +0300

    Integrate local notifications in MauiProgram
    Added local notification support for Android with a new notification channel for orders.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,6 @@
 using BistroPOS.Mobile.Services;
 using Microsoft.Extensions.Logging;
+using Plugin.LocalNotification;
 
 namespace BistroPOS.Mobile
 {
@@ -10,15 +11,25 @@ namespace BistroPOS.Mobile
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseLocalNotification(config =>
+                {
+                    config.AddAndroid(android =>
+                    {
+                        android.AddChannel(new NotificationChannelRequest
+                        {
+                            Id = "orders",
+                            Name = "الطلبات",
+                            Description = "إشعارات الطلبات الجديدة"
+                        });
+                    });
+                })
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
             // إضافة خدمات التطبيق
             builder.Services.AddSingleton<ApiService>();
-
             return builder.Build();
         }
     }
